6f06d1967fd23f98774bdf4d08db6aeab67ca9b3
Josimar Renepont <jnrenepont@hotmail.com>
2023-05-10 00:08:31 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
+
+
 
 @Service
 public class GameService {
@@ -15,6 +19,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true) // assegurando que eu não vou fazer nenhuma operação de escrita, não bloqueando o banco 
+	public GameDTO FindById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true) // assegurando que eu não vou fazer nenhuma operação de escrita, não bloqueando o banco
 	public List<GameMinDTO> findAll(){
 		List<Game>result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList(); // MAP TRANSFORMAR OBJETOS DE UMA COISA PARA OUTRA
