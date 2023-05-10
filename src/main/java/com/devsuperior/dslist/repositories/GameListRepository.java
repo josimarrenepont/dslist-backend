c8e4d5b30999425468d98702cdd7c94cfc65e624
Josimar Renepont <jnrenepont@hotmail.com>
2023-05-10 01:07:05 -0300

    GameListDTO, GameListService, GameMinProjection

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -6,4 +6,5 @@ import com.devsuperior.dslist.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long>{
 
+	
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
+import com.devsuperior.dslist.repositories.GameListRepository;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 
@@ -32,5 +34,10 @@ public class GameService {
 		
 	}
 	
+	@Transactional(readOnly = true) // assegurando que eu não vou fazer nenhuma operação de escrita, não bloqueando o banco
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection>result = gameRepository.searchByList(listId);
+		return result.stream().map(x -> new GameMinDTO(x)).toList(); // MAP TRANSFORMAR OBJETOS DE UMA COISA PARA OUTRA
+	}
 	
 }
