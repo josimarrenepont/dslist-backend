c9be83664268df31e73bb6ef7d5ffe978e81f165
josimarrenepont <jnrenepont@hotmail.com>
2024-11-15 19:08:08 -0300

    Ajusted method findById and findByList

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.controllers.Optional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
+import com.devsuperior.dslist.services.exceptions.ResourceNotFoundException;
 
 
 
@@ -20,23 +22,29 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	@Transactional(readOnly = true) // assegurando que eu não vou fazer nenhuma operação de escrita, não bloqueando o banco 
+	@Transactional(readOnly = true) 
 	public GameDTO FindById(Long id) {
-		Game result = gameRepository.findById(id).get();
-		return new GameDTO(result);
+		Game game = gameRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Game not foun with id: " + id));
+		
+		return new GameDTO(game);
 	}
 	
-	@Transactional(readOnly = true) // assegurando que eu não vou fazer nenhuma operação de escrita, não bloqueando o banco
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game>result = gameRepository.findAll();
-		return result.stream().map(x -> new GameMinDTO(x)).toList(); // MAP TRANSFORMAR OBJETOS DE UMA COISA PARA OUTRA
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 	}
 	
-	@Transactional(readOnly = true) // assegurando que eu não vou fazer nenhuma operação de escrita, não bloqueando o banco
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findByList(Long listId){
-		List<GameMinProjection>result = gameRepository.searchByList(listId);
-		return result.stream().map(x -> new GameMinDTO(x)).toList(); // MAP TRANSFORMAR OBJETOS DE UMA COISA PARA OUTRA
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		
+		if(result.isEmpty()) {
+			throw new ResourceNotFoundException("No games foound for list with id: " + listId);
+		}
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 	
 }
