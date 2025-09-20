ae38830cf79338518f72793e9aafad58fd1e9700
josimarrenepont <jnrenepont@hotmail.com>
2025-09-19 23:26:34 -0300

    ajusted pom.xml

--- src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
+++ src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
@@ -3,16 +3,19 @@ package com.devsuperior.dslist;
 import net.bytebuddy.utility.dispatcher.JavaDispatcher;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest
 class DslistApplicationTests {
 
-	@JavaDispatcher.Container
 	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14")
 			.withDatabaseName("dslist")
 			.withUsername("postgres")
-			.withPassword("07111519");
+			.withPassword("2801190225");
 
 	@DynamicPropertySource
 	static void configureProperties(DynamicPropertyRegistry registry) {
