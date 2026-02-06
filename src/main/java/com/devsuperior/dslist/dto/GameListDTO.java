bc3545fa04440b56161b2cad8d9229579c071f97
josimarrenepont <jnrenepont@hotmail.com>
2026-02-05 23:20:15 -0300

    ix: adjust GameListDTO constructor to fix compilation error

--- src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameListDTO.java
@@ -1,8 +1,13 @@
 package com.devsuperior.dslist.dto;
 
+
 import com.devsuperior.dslist.entities.GameList;
 
 public record GameListDTO (
 		Long id,
 		String name
-){}
+){
+	public GameListDTO(GameList entity) {
+		this(entity.getId(), entity.getName());
+	}
+}
